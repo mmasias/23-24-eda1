048d7240e54a104eedce87f94105b0e0a4998187
cesar <cesar.garcia@alumnos.uneatlantico.es>
2023-09-20 10:50:10 +0200

    Tabla finalizada

--- entregas/cesarGarcia/Reto-001/src/Main.java
+++ entregas/cesarGarcia/Reto-001/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
        int columnas = 10;
        String[][] matriz = new String[filas][columnas];
          inicializarMatriz(matriz);
-            imprimirMatriz(matriz);
+         opciones(matriz);
     }
     public static void inicializarMatriz(String[][] matriz){
         for (int i = 0; i < matriz.length; i++) {
@@ -15,9 +15,7 @@ public class Main {
             }
         }
     }
-    public static void imprimirMatriz(String[][] matriz) {
-        int maxCeldaLength = 7;
-
+    public static void imprimirMatriz(String[][] matriz, int maxCeldaLength) {
         System.out.print("   ");
         for (char c = 'A'; c <= 'J'; c++) {
             System.out.print("| " + c + "       ");
@@ -41,12 +39,75 @@ public class Main {
                     contenido = matriz[i][j];
                 }
 
-                if (contenido.length() > maxCeldaLength) {
-                    contenido = contenido.substring(0, maxCeldaLength);
-                }
-                System.out.print("       " + contenido + " |");
+                contenido = String.format("%-" + maxCeldaLength + "s", contenido);
+
+                System.out.print(" " + contenido + " |");
             }
             System.out.println();
+        }
+    }
+
+    public static void opciones(String[][] matriz) {
+        Scanner scanner = new Scanner(System.in);
+        int filaActual = 0;
+        int columnaActual = 0;
+        int maxCeldaLength = 7;
+
+        while (true) {
+            imprimirMatriz(matriz, maxCeldaLength);
+
+            System.out.println("\nOpciones:");
+            System.out.println("W - Mover arriba");
+            System.out.println("A - Mover izquierda");
+            System.out.println("S - Mover abajo");
+            System.out.println("D - Mover derecha");
+            System.out.println("E - Agregar texto");
+            System.out.println("Q - Salir");
+
+            System.out.println("Posición actual: Fila " + (filaActual + 1) + ", Columna " + (char) ('A' + columnaActual));
+
+            System.out.print("Ingrese una opción: ");
+            String opcion = scanner.nextLine().toUpperCase();
+
+            switch (opcion) {
+                case "W":
+                    if (filaActual > 0) {
+                        filaActual--;
+                    }
+                    break;
+                case "A":
+                    if (columnaActual > 0) {
+                        columnaActual--;
+                    }
+                    break;
+                case "S":
+                    if (filaActual < 9) {
+                        filaActual++;
+                    }
+                    break;
+                case "D":
+                    if (columnaActual < 9) {
+                        columnaActual++;
+                    }
+                    break;
+                case "E":
+                    System.out.print("Texto a agregar: ");
+                    String texto = scanner.nextLine();
+
+                    if (texto.length() > maxCeldaLength) {
+                        texto = texto.substring(0, maxCeldaLength);
+                    }
+
+                    matriz[filaActual][columnaActual] = texto;
+                    break;
+                case "Q":
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
         }
     }
 
