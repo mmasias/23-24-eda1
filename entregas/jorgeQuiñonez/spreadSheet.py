5aaadc819dd52953525d52ef9b2bd9a22928e920
jorge.quinonez <jorge.quinonez@alumnos.uneatlantico.es>
2023-09-20 16:47:19 +0200

    feature: sheet printing with rows numbers & cursor moving controls

--- entregas/jorgeQuiñonez/spreadSheet.py
+++ entregas/jorgeQuiñonez/spreadSheet.py
@@ -1,21 +1,59 @@
+import numpy as np
 
-global columns
-global rows
+sheet = np.zeros((10, 15), dtype=int)
+cursor = [0, 0]
+columnNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+rowNames = ["01", "02", "03", "04", "05", "06", "07",
+            "08", "09", "10", "11", "12", "13", "14", "15"]
 
 
-def insertText():
-    text = input().toString()
-    return text
+def run():
+    printBorders()
+    printSheet()
+    while cursorControl():
+        printBorders()
+        printSheet()
+
+
+def cursorControl():
+    userInput = input()
+    if userInput == "w":
+        cursor[0] -= 1
+    elif userInput == "s":
+        cursor[0] += 1
+    elif userInput == "a":
+        cursor[1] -= 1
+    elif userInput == "d":
+        cursor[1] += 1
+    elif userInput == "q":
+        return False
+    return True
+
+
+def printBorders():
+    print("       ", end="")
+    for i in range(1):
+        for j in range(10):
+            print(columnNames[j] + "      ", end="")
+        print()
+    print("     ", end="")
+    for i in range(10):
+        print("+-------", end="")
+    print("+")
 
 
 def printSheet():
     columns = [""] * 15
     rows = [""] * 10
+    for i in range(15):
+        print(rowNames[i] + "   ", end="")
+        for j in range(10):
+            if i == cursor[0] and j == cursor[1]:
+                print("|[     ]", end="")
+            else:
+                print("|       ", end="")
+        print("|")
+    printBorders()
 
-    for i in rows:
-        for j in columns:
-            print("       ", end="|")
-        print()
 
-
-printSheet()
+run()
