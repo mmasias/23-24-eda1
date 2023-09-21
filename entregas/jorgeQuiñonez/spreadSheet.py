cde9e81b266ab5d7906a5f18828c9b9a1125378c
jorge.quinonez <jorge.quinonez@alumnos.uneatlantico.es>
2023-09-21 21:55:20 +0200

    feature: compleated functionality of cheatsheet & instructions

--- entregas/jorgeQuiñonez/spreadSheet.py
+++ entregas/jorgeQuiñonez/spreadSheet.py
@@ -1,36 +1,37 @@
 
 sheet = [["" for x in range(10)] for y in range(15)]
 cursor = [0, 0]
-cellLenght = 7
+cellLength = 7
+leftSpacing = 5
 columnNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-rowNames = ["01", "02", "03", "04", "05", "06", "07",
-            "08", "09", "10", "11", "12", "13", "14", "15"]
+rowNames = ["1", "2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "11", "12", "13", "14", "15"]
 
 
 def run():
-    showInstructions()
 
     printColumnIndex()
     printBorders()
     printSheet()
+    showInstructions()
 
     while cursorControl():
         printColumnIndex()
         printBorders()
         printSheet()
+        showInstructions()
 
 
 def showInstructions():
-    print("---------------------------------------------INSTRUCTIONS---------------------------------------------")
-    print("The program works with lower or capital letters")
-    print("To move the cursor in the cheatsheet use: W, A, S, D keys", end="\n")
-    print("To edit cursor's actual cell press E", end="\n")
-    print("To exit the program press Q", end="\n")
+    print("Use W, A, S, D keys to move.", end="\n")
+    print("To edit cursor's actual cell press 'E'", end="\n")
+    print("To exit the program press 'Q'", end="\n")
     print()
 
 
 def cursorControl():
-    userInput = input("Ingrese una tecla: ").lower()
+    userInput = input("Ingrese un control del cursor: ").lower()
+    print()
     if userInput == "w":
         cursor[0] -= 1
         if (cursor[0] < 0):
@@ -55,43 +56,52 @@ def cursorControl():
 
 
 def printColumnIndex():
-    print("       ", end="")
+    print(" " * cellLength, end="")
     for i in range(1):
         for j in range(10):
-            print(columnNames[j] + "       ", end="")
+            print(columnNames[j] + " " * cellLength, end="")
         print()
 
 
 def printBorders():
-    print("     ", end="")
+    print(" " * leftSpacing, end="")
     for i in range(10):
         print("+-------", end="")
     print("+")
 
 
 def printSheet():
-    columns = [""] * 15
-    rows = [""] * 10
-    for i in range(15):
-        print(rowNames[i] + "   ", end="")
-        for j in range(10):
+    for i in range(len(rowNames)):
+        if (len(rowNames[i]) < 2):
+            print(rowNames[i] + " " * (leftSpacing - 1), end="")
+        else:
+            print(rowNames[i] + " " * (leftSpacing - 2), end="")
+        for j in range(len(columnNames)):
             value = sheet[i][j]
             if (value):
-                if (len(value) > cellLenght - 2):
-                    value = value[:cellLenght]
-                print("|" + str(value), end="")
+                if (len(value) > cellLength):
+                    value = value[:cellLength]
+                    print("|" + str(value), end="")
+                elif (len(value) <= cellLength):
+                    print("|" + str(value) + " " *
+                          (cellLength - len(value)), end="")
             elif i == cursor[0] and j == cursor[1]:
-                print("|" + "[" + " "*(cellLenght - 2) + "]", end="")
+                print("|" + "[" + " "*(cellLength - 2) + "]", end="")
             else:
-                print("|" + " " * cellLenght, end="")
+                print("|" + " " * cellLength, end="")
         print("|")
     printBorders()
-    print("Cursor position: " + str(cursor))
+    printActualCell(cursor[0], cursor[1])
 
 
-def editCell(x, y):
+def editCell(column, row):
     insertedText = input("Escriba para editar el contenido de la celda: ")
-    sheet[x][y] = str(insertedText)
+    sheet[column][row] = str(insertedText)
+    print()
+
+
+def printActualCell(row, column):
+    print("Celda actual =>  [" + columnNames[column] + rowNames[row] + "]")
 
 
 run()
