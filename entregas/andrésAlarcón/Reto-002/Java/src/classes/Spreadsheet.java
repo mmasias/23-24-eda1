c675dfe93ac5a600c92a39b03721369717b14441
AndrextA <andrextalarcon@gmail.com>
2023-09-29 12:59:12 +0200

    fix: display cells

--- entregas/andrésAlarcón/Reto-002/Java/src/classes/Spreadsheet.java
+++ entregas/andrésAlarcón/Reto-002/Java/src/classes/Spreadsheet.java
@@ -20,7 +20,26 @@ public class Spreadsheet {
   }
 
   public void display() {
-    // [ ... ] (same as before)
+    System.out.print("  ");
+    for (int col = 0; col < cells[0].length; col++) {
+      System.out.printf("%7s ", convertColumnToLetter(col));
+    }
+    System.out.println("");
+    System.out.println("   +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
+
+    for (int i = 0; i < cells.length; i++) {
+      System.out.printf("%2d ", (i + 1));
+      for (int j = 0; j < cells[i].length; j++) {
+        if (i == currentRow && j == currentColumn) {
+          System.out.print("|[_____]");
+        } else {
+          System.out.printf("|%7s", cells[i][j]);
+        }
+      }
+      System.out.println("|");
+    }
+
+    System.out.println("   +-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
   }
 
   public void moveUp() {
@@ -65,6 +84,11 @@ public class Spreadsheet {
 
   public String getCurrentColumnLetter() {
     char letter = (char) ('A' + currentColumn);
+    return String.valueOf(letter);
+  }
+
+  private String convertColumnToLetter(int column) {
+    char letter = (char) ('A' + column);
     return String.valueOf(letter);
   }
 }
