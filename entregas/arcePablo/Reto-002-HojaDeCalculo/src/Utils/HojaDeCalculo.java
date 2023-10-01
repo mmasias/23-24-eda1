22c6c532898b049f96085a2fa88de25623235f43
pablarce <pablo.arce1@alumnos.uneatlantico.es>
2023-10-01 18:55:29 +0200

    Sheet display fix

--- entregas/arcePablo/Reto-002-HojaDeCalculo/src/Utils/HojaDeCalculo.java
+++ entregas/arcePablo/Reto-002-HojaDeCalculo/src/Utils/HojaDeCalculo.java
@@ -9,19 +9,18 @@ public class HojaDeCalculo {
 
     public HojaDeCalculo() {
         sheet = new Sheet(18, 11);
-        movement = new Movement("A1");
         input = new Scanner(System.in);
     }
 
     public void run() {
         while (true) {
-            Menu.showSheet(sheet, movement.getCurrentCell());
-            Menu.showMovementMenu();
+            Menu.showSheet(sheet);
+            Menu.showMovementMenu(sheet.getCurrentCell());
             String command = input.nextLine();
             if (command.equals("q")) {
                 break;
             } else if (command.equals("e")) {
-                System.out.println("Ingrese el nuevo valor de la celda " + movement.getCurrentCell() + ":");
+                System.out.println("Ingrese el nuevo valor de la celda " + sheet.getCurrentCell() + ":");
                 String newValue = input.nextLine();
                 sheet.setCellValue(movement.getCurrentRow(), movement.getCurrentColumn(), newValue);
             } else {
--- entregas/arcePablo/Reto-002-HojaDeCalculo/src/Utils/Sheet.java
+++ entregas/arcePablo/Reto-002-HojaDeCalculo/src/Utils/Sheet.java
@@ -2,13 +2,58 @@ package Utils;
 
 class Sheet {
     private String[][] sheetData;
+    private String currentCell;
 
     public Sheet(int rows, int columns) {
         sheetData = new String[rows][columns];
+        currentCell = "A1";
     }
 
-    public String getCellValue(int row, int col) {
-        return sheetData[row][col];
+    public String getCellContent(int i, int j) {
+        final int spacing = 6;
+
+        if (i == 0 && j == 0) {
+            return (" ").repeat(spacing);
+        } else if (i == 0) {
+            return " " + String.valueOf((char) (j + 64)) + (" ").repeat(spacing - 1);
+
+        } else if (i == 1) {
+            if (j == 0) {
+                return "+" + ("-").repeat(spacing - 1);
+            } else if (j == this.sheetData[i].length - 1) {
+                return "+" + ("-").repeat(spacing) + "+";
+            } else {
+                return "+" + ("-").repeat(spacing);
+            }
+
+        } else if (i == this.sheetData.length - 1) {
+            return ("-").repeat(spacing) + "+";
+
+        } else if (j == 0) {
+            int nSpace = 0;
+            if (i < 11) {
+                nSpace = spacing - 1;
+            } else {
+                nSpace = spacing - 2;
+            }
+            return String.valueOf(i - 1) + (" ").repeat(nSpace) + "|";
+
+        } else if (getCellName(i - 1, j).equals(this.currentCell)) {
+            return "[" + (" ").repeat(spacing - 2) + "]" + "|";
+        } else {
+            if (this.sheetData[i][j] == null) {
+                return (" ").repeat(spacing) + "|";
+            } else {
+                return this.sheetData[i][j];
+            }
+        }
+    }
+    static String getCellName(int i, int j) {
+        return String.valueOf((char) (j + 64)) + String.valueOf(i);
+    }
+
+    public String getCurrentCell() {
+        return currentCell;
     }
 
     public void setCellValue(int row, int col, String value) {
