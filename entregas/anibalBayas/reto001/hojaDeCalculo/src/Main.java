5c03bb7c64c7d49ac09afcf88818f266db6ae45b
anibalbg <anibal.bayas@alumnos.uneatlantico.es>
2023-10-01 21:55:19 +0200

    HojaDeCalculo editado

--- entregas/anibalBayas/reto001/hojaDeCalculo/src/Main.java
+++ entregas/anibalBayas/reto001/hojaDeCalculo/src/Main.java
@@ -1,154 +1,104 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int posX = 0;
+        int posY = 0;
+        int filas = 15;
+        int columnas = 10;
 
-        Scanner entrada = new Scanner(System.in);
-        char inputUsuario;
-        boolean terminar = false;
+        String[][] hojaCalculo = new String[filas][columnas];
 
-
-        String[][] unaMatriz = {
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
-                {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "}
-
-
-        };
-
-        int[] posicionCursor = {0, 0};
-
-        int limiteInferior = 0;
-        int limiteSuperior = unaMatriz.length - 1;
-        int limiteIzquierdo = 0;
-        int limiteDerecho = unaMatriz[0].length - 1;
-        int skin = 0;
-
-        do {
-            System.out.println("+---------------------------------------------------------------------------------+");
-            System.out.println("| |   A   |   B   |   C   |   D   |   E   |   F   |   G   |   H   |   I   |   J   |");
-            System.out.println("+---------------------------------------------------------------------------------+");
-            imprimeTerreno(unaMatriz, posicionCursor);
-            System.out.println("CMDs: [W] Arriba | [S] Abajo | [A] Izquierda | [D] Derecha | [E] Editar | [F] Salir");
-            System.out.print("Ingrese un comando: ");
-
-            inputUsuario = entrada.nextLine().charAt(0);
-            switch (inputUsuario) {
-                case 's', 'S':
-                    posicionCursor[0] = posicionCursor[0] + 1;
-                    break;
-                case 'w', 'W':
-                    posicionCursor[0] = posicionCursor[0] - 1;
-                    break;
-                case 'a', 'A':
-                    posicionCursor[1] = posicionCursor[1] - 1;
-                    break;
-                case 'd', 'D':
-                    posicionCursor[1] = posicionCursor[1] + 1;
-                    break;
-                case 'E', 'e':
-                    modificar(unaMatriz, posicionCursor);
-
-                    break;
-                case 'q', 'Q':
-                    terminar = true;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                hojaCalculo[i][j] = "       ";
             }
-
-            if (posicionCursor[0] < limiteInferior) {
-                posicionCursor[0] = limiteSuperior;
-            }
-            if (posicionCursor[0] > limiteSuperior) {
-                posicionCursor[0] = limiteInferior;
-            }
-            if (posicionCursor[1] < limiteIzquierdo) {
-                posicionCursor[1] = limiteDerecho;
-            }
-            if (posicionCursor[1] > limiteDerecho) {
-                posicionCursor[1] = limiteIzquierdo;
-            }
+        }
 
 
-        } while (!terminar);
-    }
-
-    static void imprimeTerreno(String[][] unaMatriz, int[] posicionCursor) {
-        final int ANCHO_DE_CELDA = 7;
-        String celda = "";
-        limpiarPantalla();
-
-        for (int i = 0; i < unaMatriz.length; i++) {
-            if (i < 9) {
-                System.out.print(" " + (i + 1));
-            } else {
-                System.out.print((i + 1));
-            }
-
-            for (int j = 0; j < unaMatriz[i].length; j++) {
-                boolean borde = false;
-                if (posicionCursor[0] == i && posicionCursor[1] == j) {
-                    borde = true;
+        System.out.println("    A       B       C       D       E       F       G       H       I       J");
+        System.out.println("+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
+        for (int i = 0; i < filas; i++) {
+            System.out.print("|");
+            for (int j = 0; j < columnas; j++) {
+                if (i == posX && j == posY) {
+                    System.out.print("[     ]");
+                } else {
+                    System.out.print(hojaCalculo[i][j]);
                 }
-                celda = limpia(unaMatriz[i][j], ANCHO_DE_CELDA, borde);
-                System.out.print("|" + celda);
+                if (j < columnas - 1) {
+                    System.out.print("|");
+                }
             }
             System.out.println("|");
-
         }
-        System.out.println("x: " + posicionCursor[0] + " | y:" + posicionCursor[1]);
+        System.out.println("+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
+        System.out.println("Estás en la celda: " + (posX + 1) + "/" + (posY + 1));
+        System.out.println("Pulse W,A,S,D para desplazarse.");
+        System.out.println("Pulse E para editar la celda seleccionada.");
+        System.out.println("Pulse Q para salir.");
 
+        Scanner scanner = new Scanner(System.in);
 
-    }
+        while (true) {
 
-    static String limpia(String texto, int largo, boolean borde) {
-        if (texto.length() < largo) {
-            texto = " ".repeat(largo - texto.length()) + texto;
-        } else {
-            texto = texto.substring(texto.length() - largo);
-        }
-        if (borde) {
-            texto = "[" + limpia(texto, 5, !borde) + "]";
-        }
-        return texto;
-    }
+            char movimiento = scanner.next().charAt(0);
 
-    static void imprimePacman(int skin) {
-        String[] pacman = {" - "};
-        System.out.print(pacman[skin]);
-    }
-
-    static void limpiarPantalla() {
-
-        System.out.print("\033[H\033[2J");
-    }
-
-    static void modificar(String[][] hoja, int[] posicion) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese un valor");
-        String valor = sc.nextLine();
-        String resultado = "";
-        if (valor.length() > 7) {
-            for (int i = 0; i < 7; i++) {
-                resultado = resultado + valor.charAt(i);
+            switch (movimiento) {
+                case 'a', 'A':
+                    if (posY > 0)
+                        posY--;
+                    break;
+                case 'w', 'W':
+                    if (posX > 0)
+                        posX--;
+                    break;
+                case 'd', 'D':
+                    if (posY < columnas - 1)
+                        posY++;
+                    break;
+                case 's', 'S':
+                    if (posX < filas - 1)
+                        posX++;
+                    break;
+                case 'e', 'E':
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Ingrese un valor:");
+                    String valor = sc.nextLine();
+                    valor = valor.length() > 7 ? valor.substring(0, 7) : valor;
+                    hojaCalculo[posX][posY] = String.format("%7s", valor);
+                    break;
+                case 'q', 'Q':
+                    System.out.println("Programa finalizado.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Entrada no válida.");
+                    break;
             }
 
-        } else {
-            resultado = valor;
+
+            System.out.println("    A       B       C       D       E       F       G       H       I       J");
+            System.out.println("+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
+            for (int i = 0; i < filas; i++) {
+                System.out.print("|");
+                for (int j = 0; j < columnas; j++) {
+                    if (i == posX && j == posY) {
+                        System.out.print("[     ]");
+                    } else {
+                        System.out.print(hojaCalculo[i][j]);
+                    }
+                    if (j < columnas - 1) {
+                        System.out.print("|");
+                    }
+                }
+                System.out.println("|");
+            }
+            System.out.println("+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+");
+            System.out.println("Estás en la celda: " + (posX + 1) + "/" + (posY + 1));
+            System.out.println("Pulse W,A,S,D para desplazarse.");
+            System.out.println("Pulse E para editar la celda seleccionada.");
+            System.out.println("Pulse Q para salir.");
         }
-        hoja[posicion[0]][posicion[1]] = resultado;
     }
-
-
 }
