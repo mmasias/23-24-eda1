fb66329b40f8659b221d7e46882003daeddaba10
cesar <cesar.garcia@alumnos.uneatlantico.es>
2023-10-18 12:06:12 +0200

    reto-003 creado y clases actualizadas

---
+++ entregas/cesarGarcia/Reto-003/src/basica/Ejemplo.java
@@ -0,0 +1,38 @@
+package basica;
+
+class Ejemplo {
+    public static void main(String[] args) {
+
+        List nephews = new List();
+
+        viewLine(nephews);
+        System.out.println("Esta vacía? " + nephews.isEmpty());
+
+        nephews.insert("Juanito");
+        nephews.insert("Pepito");
+        nephews.insert("Anita");
+
+        System.out.println("Esta vacía? " + nephews.isEmpty());
+
+        viewLine(nephews);
+
+        nephews.insert("Luisito");
+
+        System.out.println("Tamaño: " + nephews.size());
+
+        nephews.delete();
+        System.out.println("Tamaño: " + nephews.size());
+
+        viewLine(nephews);
+    }
+
+    static void viewLine(List filaDeSobrinos) {
+
+        String[] larray = filaDeSobrinos.listAll();
+        System.out.println("LA FILA" + "-".repeat(18));
+        for (String sobrino : larray){
+            System.out.println(sobrino);
+        }
+        System.out.println("-".repeat(25));
+    }
+}
---
+++ entregas/cesarGarcia/Reto-003/src/basica/List.java
@@ -0,0 +1,76 @@
+package basica;
+
+public class List {
+    private Node first = null;
+    private String lastDeleted = null;
+
+    public int size() {
+        if (this.first == null) {
+            return 0;
+        } else {
+            int count = 1;
+            Node iterator = first;
+            while (iterator.getNext() != null) {
+                count++;
+                iterator = iterator.getNext();
+            }
+            return count;
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    public void insert(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null) {
+            this.first = newNode;
+        } else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(newNode);
+        }
+        System.out.println("Entró " + value);
+    }
+
+    public void delete() {
+        if (this.first != null) {
+            Node iterator = this.first;
+            Node previous = null;
+            while (iterator.getNext() != null) {
+                previous = iterator;
+                iterator = iterator.getNext();
+            }
+            if (previous != null) {
+                previous.setNext(null);
+                lastDeleted = iterator.getValue();
+                System.out.println("Salió " + lastDeleted);
+            } else {
+                lastDeleted = this.first.getValue();
+                this.first = null;
+                System.out.println("Salió " + lastDeleted);
+            }
+        }
+    }
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+
+    public String getLastDeleted() {
+        return lastDeleted;
+    }
+}
