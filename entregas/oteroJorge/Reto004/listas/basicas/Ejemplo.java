5d1a3cb707696c507f0a12fc3602dc702191706b
Jorge Otero <114479616+Jorgeog25@users.noreply.github.com>
2023-10-22 21:23:04 +0200

    Create Ejemplo.java

---
+++ entregas/oteroJorge/Reto004/listas/basicas/Ejemplo.java
@@ -0,0 +1,42 @@
+package listas.basica;
+
+class Ejemplo {
+    public static void main(String[] args) {
+
+        List nephews = new List();
+
+        viewLine(nephews);
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+        nephews.insertEnd("Juanito");
+        nephews.insertEnd("Pepito");
+        nephews.insertEnd("Anita");
+
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+        viewLine(nephews);
+
+        nephews.insertEnd("Luisito");
+        nephews.insertFront("Jaimito");
+        nephews.insertExactlyPosition(2, "Pedrito");
+
+        System.out.println("Tamaño: " + nephews.size());
+
+        nephews.deleteEnd();
+        nephews.deleteFront();
+
+        System.out.println("Tamaño: " + nephews.size());
+
+        viewLine(nephews);
+    }
+
+    static void viewLine(List filaDeSobrinos) {
+
+        String[] larray = filaDeSobrinos.listAll();
+        System.out.println("LA FILA" + "-".repeat(18));
+        for (String sobrino : larray){
+            System.out.println(sobrino);
+        }
+        System.out.println("-".repeat(25));
+    }
+}
