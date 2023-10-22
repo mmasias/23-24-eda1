37fbef111411404d00d66b3faa9a077e23a02f9a
Jorge Otero <114479616+Jorgeog25@users.noreply.github.com>
2023-10-22 21:23:33 +0200

    Add files via upload

---
+++ entregas/oteroJorge/Reto004/listas/basicas/List.java
@@ -0,0 +1,101 @@
+package listas.basica;
+
+public class List {
+
+    private Node first = null;
+
+    public int size() {
+        int count = 0;
+        Node iterator = first;
+
+        while (iterator != null) {
+            count++;
+            iterator = iterator.getNext();
+        }
+
+        return count;
+    }
+    public boolean isEmpty() {
+        return this.size() > 0 ? false : true;
+    }
+
+    public void insertEnd(String value) {
+        Node nodoQueEntra = new Node(value);
+        if (this.first == null){
+            this.first = nodoQueEntra;
+        } else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(nodoQueEntra);
+        }
+        System.out.println("> Llegó " + value);
+    }
+    public void insertFront(String value) {
+        Node n = new Node(value);
+        if (this.first == null) {
+        } else {
+            n.setNext(this.first);
+        }
+        this.first = n;
+    }
+    public void insertExactlyPosition(int position, String value){
+        Node n = new Node(value);
+        if (this.first == null) {
+            this.first = n;
+        } else {
+            Node iterator = this.first;
+            int count = 0;
+            while (iterator.getNext() != null && count < position - 1) {
+                iterator = iterator.getNext();
+                count++;
+            }
+            n.setNext(iterator.getNext());
+            iterator.setNext(n);
+        }
+    }
+
+    public void deleteEnd() {
+        if (this.first == null) {
+            return;
+        }
+
+        Node iterator = this.first;
+        Node previous = null;
+
+        while (iterator.getNext() != null) {
+            previous = iterator;
+            iterator = iterator.getNext();
+        }
+
+        if (previous == null) {
+            this.first = null;
+        } else {
+            previous.setNext(null);
+        }
+        System.out.println("> Se fue " + iterator.getValue());
+    }
+    public void deleteFront() {
+        if (this.first == null) {
+            return;
+        }
+        Node iterator = this.first;
+        this.first = iterator.getNext();
+        System.out.println("> Se fue " + iterator.getValue());
+    }
+
+    public String[] listAll() {
+        String[] list = new String[this.size()];
+        Node iterator = this.first;
+        if (iterator == null)
+            return list;
+        int count = 0;
+        while (iterator != null) {
+            list[count] = iterator.getValue();
+            count++;
+            iterator = iterator.getNext();
+        }
+        return list;
+    }
+}
