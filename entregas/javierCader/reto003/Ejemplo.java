60ae6df78148444c1a2858e02c364a8cc35859df
Hexcss <cadersuay.javier@gmail.com>
2023-10-23 12:50:49 +0200

    feat: Added Reto-004 Javier Cader

--- entregas/javierCader/reto003/Ejemplo.java
+++ entregas/javierCader/reto003/Ejemplo.java
@@ -4,13 +4,19 @@ class Ejemplo {
         List nephews = new List();
 
         viewLine(nephews);
-        System.out.println("Esta vacia? " + nephews.isEmpty());
-        
+        System.out.println("¿Está vacía? " + nephews.isEmpty());
+
         System.out.println(" > Llegó " + nephews.insert("Juanito"));
         System.out.println(" > Llegó " + nephews.insert("Pepito"));
         System.out.println(" > Llegó " + nephews.insert("Anita"));
 
-        System.out.println("Esta vacia? " + nephews.isEmpty());
+        System.out.println(" > Llegó al inicio " + nephews.insertFirst("Pablito"));
+
+        System.out.println(" > Llegó al final " + nephews.insertLast("Raulito"));
+
+        System.out.println(" > Llegó en la posición 2 " + nephews.insertAt("Karol", 1));
+
+        System.out.println("¿Está vacía? " + nephews.isEmpty());
 
         viewLine(nephews);
 
@@ -22,16 +28,24 @@ class Ejemplo {
         System.out.println("Tamaño: " + nephews.size());
 
         viewLine(nephews);
+
+        System.out.println("Se fue desde el inicio " + nephews.deleteFirst());
+        viewLine(nephews);
+
+        System.out.println("Se fue desde el final " + nephews.deleteLast());
+        viewLine(nephews);
+
+        System.out.println("Se fue desde la posición 2 " + nephews.deleteAt(1));
+        viewLine(nephews);
     }
 
     static void viewLine(List filaDeSobrinos) {
         
         String[] larray = filaDeSobrinos.listAll();
         System.out.println("LA FILA" + "-".repeat(18));
-        for (String sobrino : larray){
+        for (String sobrino : larray) {
             System.out.println(sobrino);
         }
         System.out.println("-".repeat(25));
     }
 }
- 
--- entregas/javierCader/reto003/List.java
+++ entregas/javierCader/reto003/List.java
@@ -11,6 +11,69 @@ public class List {
         return this.size == 0;
     }
 
+    public String insertFirst(String value) {
+        Node newNode = new Node(value);
+        newNode.setNext(first);
+        first = newNode;
+        size++;
+        return value;
+    }
+
+    public String insertLast(String value) {
+        return insert(value);
+    }
+
+    public String insertAt(String value, int index) {
+        if (index == 0) {
+            return insertFirst(value);
+        } else if (index == size) {
+            return insertLast(value);
+        } else if (index > 0 && index < size) {
+            Node newNode = new Node(value);
+            Node iterator = first;
+            for (int i = 0; i < index - 1; i++) {
+                iterator = iterator.getNext();
+            }
+            newNode.setNext(iterator.getNext());
+            iterator.setNext(newNode);
+            size++;
+            return value;
+        }
+        return null;
+    }
+
+    public String deleteFirst() {
+        if (first != null) {
+            String removedValue = first.getValue();
+            first = first.getNext();
+            size--;
+            return removedValue;
+        }
+        return null;
+    }
+
+    public String deleteLast() {
+        return delete(); 
+    }
+
+    public String deleteAt(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        } else if (index == size - 1) {
+            return deleteLast();
+        } else if (index > 0 && index < size - 1) {
+            Node iterator = first;
+            for (int i = 0; i < index - 1; i++) {
+                iterator = iterator.getNext();
+            }
+            String removedValue = iterator.getNext().getValue();
+            iterator.setNext(iterator.getNext().getNext());
+            size--;
+            return removedValue;
+        }
+        return null;
+    }
+
     public String insert(String value) {
         Node nodoQueEntra = new Node(value);
         if (this.first == null) {
@@ -22,7 +85,7 @@ public class List {
             }
             iterator.setNext(nodoQueEntra);
         }
-        this.size++; 
+        this.size++;
         return value;
     }
 
@@ -36,7 +99,7 @@ public class List {
                 iterator = iterator.getNext();
             }
             removedValue = iterator.getValue();
-            if (previous == null) { 
+            if (previous == null) {
                 this.first = null;
             } else {
                 previous.setNext(null);
