3536ec039d5e6c9d209cd21163adcc4e059803c8
sara-patino <sara.patino@alumnos.uneatlantico.es>
2023-10-23 22:24:26 +0200

    challange 3 completed

---
+++ entregas/saraPatino/003/src/Main.java
@@ -0,0 +1,39 @@
+package src;
+
+class Main {
+    public static void main(String[] args) {
+
+        List nephews = new List();
+
+        viewLine(nephews);
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+        nephews.insert("Juanito");
+        nephews.insert("Pepito");
+        nephews.insert("Anita");
+
+        System.out.println("Esta vacia? " + nephews.isEmpty());
+
+        viewLine(nephews);
+
+        nephews.insert("Luisito");
+
+        System.out.println("Tamaño: " + nephews.size());
+
+        nephews.delete();
+        System.out.println("Tamaño: " + nephews.size());
+
+        viewLine(nephews);
+    }
+
+    static void viewLine(List filaDeSobrinos) {
+        
+        String[] larray = filaDeSobrinos.listAll();
+        System.out.println("LA FILA" + "-".repeat(18));
+        for (String sobrino : larray){
+            System.out.println(sobrino);
+        }
+        System.out.println("-".repeat(25));
+    }
+}
+ 
