05609adf46be5eb8461eee9af37f93f5a46254f5
rubensetien <114570479+rubensetien@users.noreply.github.com>
2023-11-04 19:06:03 +0100

    Add files via upload

---
+++ Reto5/src/List.java
@@ -0,0 +1,70 @@
+public class List {
+
+    private Node first = null;
+
+    public int size() {
+        if (this.first == null) {
+            return 0;
+        } else {
+            int count = 1;
+            Node iterator = first;
+            while (iterator.getNext() != null) {
+                count++;
+                iterator = iterator.getNext();
+            }
+            return count;
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.size() > 0 ? false : true;
+    }
+
+    public void insert(int value) {
+        Node nodoQueEntra = new Node(value);
+        if (this.first == null)
+            this.first = nodoQueEntra;
+        else {
+            Node iterator = this.first;
+            while (iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            iterator.setNext(nodoQueEntra);
+        }
+
+    }
+
+    public Node  deleteMidSide(int value){
+        Node iterator= this.first;
+        Node anterior= first;
+        while (iterator!=null){
+            if(value == iterator.getValue()){
+                anterior.setNext(iterator.getNext());
+                return iterator;
+            }else{
+                anterior=iterator;
+            }
+            iterator=iterator.getNext();
+        }
+
+        return null;
+    }
+
+
+
+
+        public int[] listAll() {
+            int[] list = new int[this.size()];
+            Node iterator = this.first;
+            if (iterator == null)
+                return list;
+            int count = 0;
+            while (iterator != null) {
+                list[count] =  iterator.getValue();
+                count++;
+                iterator = iterator.getNext();
+            }
+            return list;
+        }
+
+}
