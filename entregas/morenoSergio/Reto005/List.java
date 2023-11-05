70154072d3fcdac97b1fe9f82bc3389609a5b7a2
Sergio Moreno <113471709+srgiom@users.noreply.github.com>
2023-11-05 23:23:14 +0100

    Add files via upload

---
+++ entregas/morenoSergio/Reto005/List.java
@@ -0,0 +1,47 @@
+public class List {
+
+    private Node first;
+    public int size() {
+        if (this.first == null) {
+            return 0;
+        } else {
+            int count = 1;
+            Node iterator = first;
+            while (iterator.getNext() != null) {
+                count++;
+                iterator = iterator.getNext();
+            }
+            return count;
+        }
+    }
+    public boolean isEmpty() {
+        return this.size() > 0 ? false : true;
+    }
+    public void insert(String value) {
+        Node newNode = new Node(value);
+        if (this.first == null) {
+            this.first = newNode;
+        } else {
+            newNode.setNext(this.first);
+            this.first = newNode;
+        }
+        System.out.println("> Llegó " + value);
+    }
+    public void delete() {
+        if (this.first != null) {
+            System.out.println("> Se fue " + this.first.getValue());
+            this.first = this.first.getNext();
+        }
+    }
+    public String[] listAll() {
+        String[] larray = new String[this.size()];
+        Node iterator = this.first;
+        int i = 0;
+        while (iterator != null) {
+            larray[i] = iterator.getValue();
+            iterator = iterator.getNext();
+            i++;
+        }
+        return larray;
+    }
+}
