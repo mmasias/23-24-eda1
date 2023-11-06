ce4c84367e9e44df603ae39d331c81126e2bfd98
Adrián García Arranz <114479608+aadrigaar@users.noreply.github.com>
2023-11-06 22:23:35 +0100

    Add files via upload

---
+++ entregas/adrianGarcia/Reto005/NodoCola.java
@@ -0,0 +1,14 @@
+class NodoCola extends Node<Integer> {
+    public NodoCola(int data) {
+        super(data);
+    }
+
+    public int getData() {
+        return super.getValue();
+    }
+
+    public void setData(int data) {
+        super.setValue(data);
+    }
+
+}
