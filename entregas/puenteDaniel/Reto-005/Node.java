645f18471165033b6392afdbcec7e2f3687652d9
Dani Puente <danielpuentesaranana@gmail.com>
2023-11-06 15:10:08 +0100

    Some changes to be committed:

---
+++ entregas/puenteDaniel/Reto-005/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+    private String value;
+    private Node next;
+
+    public Node(String value) {
+        this.setValue = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setValue(String value) {
+        this.value = Newvalue;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
