5ef9da85f8599bfade8b50f8463257217dcc2144
Álvaro Lostal <alvaro.lostal@alumnos.uneatlantico.es>
2023-11-06 21:02:58 +0100

    Algunos cambios para solucionar el manejo de las probabilidades

---
+++ entregas/lostalAlvaro/reto005/src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+        List<NodoCola> colas = new List<>();
+
+        for (int i = 0; i < 4; i++) {
+            colas.add(new NodoCola(0));
+        }
+
+        int cola = 0;
+
+        for (int minuto = 1; minuto <= 720; minuto++) {
+            int probLlegada = (int) ( Math.random() * 10 ) + 1;
+
+            if (probLlegada <= 6) {
+                if (cajasFull(colas)) {
+                    cola++;
+                } else {
+                    for (int i = 0; i < colas.size(); i++) {
+                        if (colas.get(i).getData() == 0) {
+                            colas.get(i).setData((int) ( Math.random() * 11 ) + 5);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < colas.size(); i++) {
+                if (colas.get(i).getData() > 0) {
+                    colas.get(i).setData(colas.get(i).getData() - 1);
+                }
+            }
+
+            if (cola > 0) {
+                for (int i = 0; i < colas.size(); i++) {
+                    if (colas.get(i).getData() == 0) {
+                        colas.get(i).setData((int) (Math.random() * 11) + 5);
+                        cola--;
+                        break;
+                    }
+                }
+            }
+
+
+            System.out.println("MINUTO " + minuto + " - " + ( probLlegada <= 4 ? "Llega 1 persona" : "No llega nadie" ) + " - En cola: " + cola);
+            System.out.print("Cajas:");
+            for (int i = 0; i < colas.size(); i++) {
+                System.out.print("[" + colas.get(i).getData() + "] ");
+            }
+            System.out.println("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+        }
+    }
+
+    private static boolean cajasFull(List<NodoCola> colas) {
+        for (int i = 0; i < colas.size(); i++) {
+            if (colas.get(i).getData() == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
