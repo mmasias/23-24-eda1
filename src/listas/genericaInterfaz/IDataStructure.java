d019ae62661500dc1a68ded81917e3f0992e8d48
Veronika Esparragoza <83025946+VeronikaEspa@users.noreply.github.com>
2023-11-30 23:48:55 +0100

    Reto-006

--- src/listas/genericaInterfaz/IDataStructure.java
+++
@@ -1,8 +0,0 @@
-package listas.genericaInterfaz;
-
-public interface IDataStructure<T> {
-
-    public boolean isEmpty();
-    public int size();
-    public Object[] listData();
-}
--- src/listas/genericaInterfaz/README.md
+++
@@ -1,9 +0,0 @@
-# CRUD de maratón
-
-## Lista genérica con interfaz
-
-![](/imagenes/modelosUML/modelo001.svg)
-
-## Implementación
-
-![](/imagenes/modelosUML/modelo002.svg)
