06020dff3705e7b0f4d114faf3b66580ba164b98
Xm3Ga <77532409+Xm3Ga@users.noreply.github.com>
2023-11-30 23:15:13 +0100

    Reto 06 upload

---
+++ entregas/adrianPérez/code/Node.java
@@ -0,0 +1,26 @@
+package code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    private String name;
+    private List<Node> children;
+
+    public Node(String name) {
+        this.name = name;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild(Node node) {
+        this.children.add(node);
+    }
+
+    public List<Node> getChildren() {
+        return this.children;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
