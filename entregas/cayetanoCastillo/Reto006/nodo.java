dd9546043197095d51ad7a7c419e8cb4aba1539f
Cayetano Castillo⚽️ <114479626+MikaOT@users.noreply.github.com>
2023-12-01 13:06:27 +0100

    Delete entregas/cayetanoCastillo/Reto006 directory

--- entregas/cayetanoCastillo/Reto006/nodo.java
+++
@@ -1,9 +0,0 @@
-public class nodo {
-    int valor;
-    nodo izquierdo, derecho;
-
-    public nodo(int valor) {
-        this.valor = valor;
-        this.izquierdo = this.derecho = null;
-    }
-}
