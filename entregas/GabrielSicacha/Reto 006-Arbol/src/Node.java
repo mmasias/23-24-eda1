e7e81740788db745458ff8f51d94eb2dfc5686eb
Gabriel Sicacha Palmar <66096194+Gbrl413@users.noreply.github.com>
2023-12-02 16:18:15 +0100

    Add files via upload

---
+++ entregas/GabrielSicacha/Reto 006-Arbol/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int value;
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int value) {
+        this.value = value;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+}
