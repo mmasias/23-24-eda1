fdc0e72b0b20e55f439d9dfc2528626e30b18394
Iker Celaya <iker.celaya@alumnos.uneatlantico.es>
2024-01-24 00:00:53 +0100

    Reto007

---
+++ entregas/celayaIker/Reto007/src/Dia.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Dia {
+
+  int numeroDia;
+  List<Ingesta> ingestas;
+
+  public Dia(int numeroDia) {
+    this.numeroDia = numeroDia;
+    this.ingestas = new ArrayList<>();
+  }
+
+  public Ingesta agregarIngesta(String tipo) {
+    Ingesta ingesta = new Ingesta(tipo);
+    ingestas.add(ingesta);
+    return ingesta;
+  }
+}
---
+++ entregas/celayaIker/Reto007/src/EncuestaNutricional.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class EncuestaNutricional {
+
+  String nombrePaciente;
+  List<Dia> diasEncuestados;
+
+  public EncuestaNutricional(String nombrePaciente) {
+    this.nombrePaciente = nombrePaciente;
+    this.diasEncuestados = new ArrayList<>();
+  }
+
+  public void agregarDia(int numeroDia) {
+    if (diasEncuestados.size() < 5) {
+      Dia dia = new Dia(numeroDia);
+      diasEncuestados.add(dia);
+    } else {
+      System.out.println("No se pueden agregar más de 5 días a la encuesta.");
+    }
+  }
+  public void registrarAlimento(int numeroDia, String tipoIngesta, String nombreAlimento) {
+    for (Dia dia : diasEncuestados) {
+      if (dia.numeroDia == numeroDia) {
+        for (Ingesta ingesta : dia.ingestas) {
+          if (ingesta.tipo.equals(tipoIngesta)) {
+            ingesta.agregarAlimento(nombreAlimento);
+            return;
+          }
+        }
+        
+        Ingesta ingesta = dia.agregarIngesta(tipoIngesta);
+        ingesta.agregarAlimento(nombreAlimento);
+        return;
+      }
+    }
+    System.out.println("No se encontró el día o tipo de ingesta especificado.");
+  }
+
+  public void mostrarEncuesta() {
+    if (diasEncuestados.isEmpty()) {
+      System.out.println("Sin datos para mostrar.");
+      return;
+    }
+
+    System.out.println("Encuesta Nutricional para " + nombrePaciente + ":");
+
+    for (Dia dia : diasEncuestados) {
+      System.out.println("\nDía " + dia.numeroDia + ":");
+
+      if (dia.ingestas.isEmpty()) {
+        System.out.println("   Sin datos de ingesta para mostrar.");
+      } else {
+        for (Ingesta ingesta : dia.ingestas) {
+          System.out.println("  " + ingesta.tipo + ":");
+          if (ingesta.alimentos.isEmpty()) {
+            System.out.println("      Sin alimentos registrados.");
+          } else {
+            for (Alimento alimento : ingesta.alimentos) {
+              System.out.println("      " + alimento.nombre);
+            }
+          }
+        }
+      }
+    }
+  }
+}
