a2cf337504945615f3532f07582c28d3bcf8d2aa
lydiaa-gr <lydia.garcia@alumnos.uneatlantico.es>
2024-01-23 22:50:18 +0100

    reto7

---
+++ entregas/garciaLydia/reto007/EncuestaNutricional.java
@@ -0,0 +1,33 @@
+package entregas.garciaLydia.reto007;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class EncuestaNutricional {
+    private String nombrePaciente;
+    private String fechaAlta;
+    private List<Dia> dias;
+
+    public EncuestaNutricional(String nombrePaciente, String fechaAlta) {
+        this.nombrePaciente = nombrePaciente;
+        this.fechaAlta = fechaAlta;
+        this.dias = new ArrayList<>();
+    }
+
+    public String getNombrePaciente() {
+        return nombrePaciente;
+    }
+
+    public String getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public List<Dia> getDias() {
+        return dias;
+    }
+
+    public void agregarDia(Dia dia) {
+        dias.add(dia);
+    }
+}
