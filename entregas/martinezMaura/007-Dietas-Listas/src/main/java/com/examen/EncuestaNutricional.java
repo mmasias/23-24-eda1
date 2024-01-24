7afc972c9e126667b14aae0a7eb29cb60ff1d9fa
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-01-24 12:52:11 +0100

    Update EncuestaNutricional.java
    Clase terminada. Verificación de Datos: Se verifica si la lista diasEncuestados está vacía.                                                                                 Impresión de Encuesta .                                                                                                        Iteración a través de los Días,Impresión de Detalles del Día,Iteración a través de las Ingestas,Verificación de Alimentos,Iteración a través de los Alimentos.

--- entregas/martinezMaura/007-Dietas-Listas/src/main/java/com/examen/EncuestaNutricional.java
+++ entregas/martinezMaura/007-Dietas-Listas/src/main/java/com/examen/EncuestaNutricional.java
@@ -21,3 +21,49 @@ class EncuestaNutricional {
       System.out.println("No se pueden agregar más de 5 días a la encuesta.");
     }
   }
+  public void registrarAlimento(int numeroDia, String tipoIngesta, String nombreAlimento) {
+    for (Dia dia : diasEncuestados) {
+      if (dia.numeroDia == numeroDia) {
+        for (Ingesta ingesta : dia.ingestas) {
+          if (ingesta.tipo.equals(tipoIngesta)) {
+            ingesta.agregarAlimento(nombreAlimento);
+            return;
+          }
+        }
+
+        Ingesta ingesta = dia.agregarIngesta(tipoIngesta);
+        ingesta.agregarAlimento(nombreAlimento);
+        return;
+      }
+    }
+    System.out.println("No se encontró el día o tipo de ingesta especificado.");
+  }
+
+  public void mostrarEncuesta() {
+    if (diasEncuestados.isEmpty()) {
+      System.out.println("No hay datos de encuesta para mostrar.");
+      return;
+    }
+
+    System.out.println("Encuesta Nutricional para " + nombrePaciente + ":");
+
+    for (Dia dia : diasEncuestados) {
+      System.out.println("\nDía " + dia.numeroDia + ":");
+
+      if (dia.ingestas.isEmpty()) {
+        System.out.println("   Sin datos de ingesta para mostrar.");
+      } else {
+        for (Ingesta ingesta : dia.ingestas) {
+          System.out.println("  " + ingesta.tipo + ":");
+          if (ingesta.alimentos.isEmpty()) {
+            System.out.println("      Sin alimentos registrados.");
+          } else {
+            for (Alimento alimento : ingesta.alimentos) {
+              System.out.println("      " + alimento.nombre);
+            }
+          }
+        }
+      }
+    }
+  }
+}
