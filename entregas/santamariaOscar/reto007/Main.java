f75572b6bfb3c70b1b9d54c525f7bd8f1b69532b
Oscar Santamaria <114479802+oscarsantasanchez@users.noreply.github.com>
2024-01-24 11:23:13 +0100

    Add files via upload

---
+++ entregas/santamariaOscar/reto007/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Gestion gestion = new Gestion();
+        gestion.capturarDatos();
+        gestion.mostrar();
+
+    }
+}
+
+/*
+ * PACIENTE
+ *    |
+ * ENCUESTA
+ *    |
+ *   DIA
+ *  |  |
+   INGESTA
+ */ 
+
