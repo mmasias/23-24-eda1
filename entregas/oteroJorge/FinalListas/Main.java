b5229ff92dbf6f9b7da69ec57bfd2958b695c883
Jorgeog25 <jrotero258@gmail.com>
2024-01-24 13:14:14 +0100

    Presentaciondel Main

--- entregas/oteroJorge/FinalListas/Main.java
+++ entregas/oteroJorge/FinalListas/Main.java
@@ -1,22 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Encuesta encuesta = new Encuesta("01/01/2020");
-        Paciente paciente = new Paciente("Jorge", "01/01/2020");
+        Paciente paciente = new Paciente(getString(nombrePaciente()), "01/01/2020");
+        boolean exit = false;
         paciente.insertarDia();
         encuesta.mostrarArbol(paciente);
         System.out.println();
-        insertarAlimentos(paciente, 1, 1);
+
+        insertarAlimentos(paciente, exit, getInt(mensajeDia()), getInt(mensajeIngesta()), getString(nombreAlimento()));
+        
         encuesta.mostrarArbol(paciente);
         System.out.println("Fin del programa");
 
+
     }
 
-    static void insertarAlimentos(Paciente paciente, int dia, int ingesta){
-        System.out.println("Insertando alimentos");
-        paciente.getDias().get(dia).getIngestas().get(ingesta).addAlimento(new Alimento("Manzana"));
-        System.out.println();
+    static void insertarAlimentos(Paciente paciente,boolean exit, int dia, int ingesta, String nombreAlimento){
+        
+        do{
+            System.out.println("Insertando alimentos");
+            paciente.getDias().get(dia).getIngestas().get(ingesta).addAlimento(new Alimento(nombreAlimento));
+            System.out.println();
+            if(getInt(mensajeSalir()) == 1){
+                exit = true;
+            }
+        }while(!exit);
+    }
 
+
+    static int getInt(String mensaje){
+        System.out.println(mensaje);
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+    static String getString(String mensaje){
+        System.out.println(mensaje);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    static String nombrePaciente(){
+        return "Ingrese el nombre del paciente";
+    }
+
+    static String nombreAlimento(){
+        return "Ingrese el nombre del alimento";
+    }
+
+    static String mensajeDia(){
+        return "Ingrese el dia: 1/2/3/4/5";
+    }
+
+    static String mensajeIngesta(){
+        return "Ingrese la ingesta: 1. Desayuno 2. Almuerzo 3. Comida 4. Merienda 5. Cena";
+    }
+
+    static String mensajeSalir(){
+        return "Desea salir? 1. Si 2. No";
     }
     
 }
