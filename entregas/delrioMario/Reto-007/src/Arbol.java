bb0125c71b05acc3effa36615fc85d4809d17502
Mariodrm17 <mario.delriomerino@alumnos.uneatlantico.es>
2024-01-24 11:26:00 +0100

    Primer commit examen final eda

---
+++ entregas/delrioMario/Reto-007/src/Arbol.java
@@ -0,0 +1,116 @@
+public class Arbol<T extends DatosArbol> {
+    private NodoArbol<T> raiz;
+
+    public Arbol(NodoArbol<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public NodoArbol<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(NodoArbol<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public void preOrden(NodoArbol<T> raiz, String nivel) {
+        if (raiz != null) {
+            System.out.println(nivel + raiz.getValor().getInfo());
+            for (int i = 0; i < raiz.getHijos().size(); i++) {
+                preOrden(raiz.getHijos().getObjeto(i), nivel + "    ");
+            }
+        }
+    }
+
+    public void postOrden(NodoArbol<T> nodo) {
+        if (nodo != null) {
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+             //   preOrden(raiz.getHijos().getObjeto(i));
+            }
+            System.out.println(nodo.getValor().getInfo());
+        }
+    }
+
+    public int totalCaloriasEnc(NodoArbol<T> nodo) {
+        if (nodo != null) {
+            int total = 0;
+            if (nodo.getValor() instanceof Ingesta) {
+                Ingesta ingesta = (Ingesta) nodo.getValor();
+                total += ingesta.totalCalorias();
+            }
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+                total += totalCaloriasEnc(nodo.getHijos().getValorPorPosicion(i));
+            }
+            return total;
+        } else {
+            return 0;
+        }
+    }
+
+    public int totalAzucarEnc(NodoArbol<T> nodo) {
+        if (nodo != null) {
+            int total = 0;
+            if (nodo.getValor() instanceof Ingesta) {
+                Ingesta ingesta = (Ingesta) nodo.getValor();
+                total += ingesta.totalAzucar();
+            }
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+                total += totalAzucarEnc(nodo.getHijos().getValorPorPosicion(i));
+            }
+            return total;
+        } else {
+            return 0;
+        }
+    }
+
+    public int totalProteinasEnc(NodoArbol<T> nodo) {
+        if (nodo != null) {
+            int total = 0;
+            if (nodo.getValor() instanceof Ingesta) {
+                Ingesta ingesta = (Ingesta) nodo.getValor();
+                total += ingesta.totalProteinas();
+            }
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+                total += totalProteinasEnc(nodo.getHijos().getValorPorPosicion(i));
+            }
+            return total;
+        } else {
+            return 0;
+        }
+    }
+
+    public int totalGrasasEnc(NodoArbol<T> nodo) {
+        if (nodo != null) {
+            int total = 0;
+            if (nodo.getValor() instanceof Ingesta) {
+                Ingesta ingesta = (Ingesta) nodo.getValor();
+                total += ingesta.totalGrasas();
+            }
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+                total += totalGrasasEnc(nodo.getHijos().getValorPorPosicion(i));
+            }
+            return total;
+        } else {
+            return 0;
+        }
+    }
+
+    public int totalTipoEnc(NodoArbol<T> nodo, String nombre) {
+        if (nodo != null) {
+            int total = 0;
+            if (nodo.getValor() instanceof Ingesta) {
+                Ingesta ingesta = (Ingesta) nodo.getValor();
+                total += ingesta.totalTipo(nombre);
+            }
+            for (int i = 0; i < nodo.getHijos().size(); i++) {
+                total += totalTipoEnc(nodo.getHijos().getValorPorPosicion(i), nombre);
+            }
+            return total;
+        } else {
+            return 0;
+        }
+
+    }
+
+
+}
