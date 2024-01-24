357e9d79906ad6754c25c0f5c7e30dba6a513356
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-01-24 11:58:48 +0100

    Update Dia.java

--- entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Dia.java
+++ entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Dia.java
@@ -1,41 +1,40 @@
 package com.escuela;
 
-import java.util.TreeMap;
-
 class Dia {
     int numeroDia;
-    TreeMap<String, Ingesta> ingestas;
+    Ingesta raiz;
+
+    Dia izquierda;
+    Dia derecha;
 
     public Dia(int numeroDia) {
         this.numeroDia = numeroDia;
-        this.ingestas = new TreeMap<>();
+        this.raiz = null;
+        this.izquierda = null;
+        this.derecha = null;
     }
 
     public void registrarAlimento(String tipoIngesta, String nombreAlimento) {
-        Ingesta ingesta = ingestas.get(tipoIngesta);
-        if (ingesta != null) {
-            ingesta.agregarAlimento(nombreAlimento);
-        } else {
-            System.out.println("No se encontró el tipo de ingesta especificado.");
+        if (raiz == null) {
+            raiz = new Ingesta(tipoIngesta);
         }
+        raiz.registrarAlimento(nombreAlimento);
     }
 
     public void mostrarDia() {
         System.out.println("\nDía " + numeroDia + ":");
-
-        if (ingestas.isEmpty()) {
-            System.out.println("   Sin datos de ingesta para mostrar.");
+        if (raiz != null) {
+            mostrarIngesta(raiz);
         } else {
-            for (Ingesta ingesta : ingestas.values()) {
-                System.out.println("  " + ingesta.tipo + ":");
-                if (ingesta.alimentos.isEmpty()) {
-                    System.out.println("      Sin alimentos registrados.");
-                } else {
-                    for (Alimento alimento : ingesta.alimentos.values()) {
-                        System.out.println("      " + alimento.nombre);
-                    }
-                }
-            }
+            System.out.println("   Sin datos de ingesta para mostrar.");
+        }
+    }
+
+    private void mostrarIngesta(Ingesta nodo) {
+        if (nodo != null) {
+            mostrarIngesta(nodo.izquierda);
+            nodo.mostrarIngesta();
+            mostrarIngesta(nodo.derecha);
         }
     }
 }
