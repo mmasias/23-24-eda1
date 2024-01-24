f0a9e0dabb7e441f95757036e00c83ee0c75e2da
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-01-24 11:58:09 +0100

    Update Main.java

--- entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Main.java
+++ entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Main.java
@@ -2,7 +2,7 @@ package com.escuela;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
