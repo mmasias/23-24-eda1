d624c526ea7778e3d62c123bf31773289eb599a8
srgiom <113471709+srgiom@users.noreply.github.com>
2024-01-24 12:02:50 +0100

    Primeros cambios

--- entregas/morenoSergio/Reto007-2/Main.java
+++ entregas/morenoSergio/Reto007-2/Main.java
@@ -1,6 +1,13 @@
+import.java.time.LocalDate;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Gestion gestion = new Gestion();
-        gestion.iniciarInterfazUsuario();
+        Scanner datos = new Scanner (System.in);
+        int numeroDia = 0;
+
+        System.out.println
     }
 }
