ae45d099e1e70c473ad59aab2aa27ff42c0c994a
aadrigaar <adrian.garcia1@alumnos.uneatlantico.es>
2024-01-24 12:11:50 +0100

    Voy haciendo cambios en diagrama UML

---
+++ entregas/adrianGarcia/ExamenFinal/Horario.java
@@ -0,0 +1,24 @@
+import java.time.LocalTime;
+import java.util.Comparator;
+import java.util.function.Consumer;
+
+public class Horario {
+    private Arbol<Evento> eventos;
+
+    public Horario() {
+        this.eventos = new Arbol<>(Comparator.comparing(Evento::getHora));
+    }
+
+    public void agregarEvento(Evento evento) {
+        eventos.insertar(evento);
+    }
+
+    public Evento buscarEvento(LocalTime hora) {
+        return eventos.buscar(new Evento(hora, ""));
+    }
+
+    public void recorrerEventos(Consumer<Evento> accion) {
+        eventos.recorrerInOrden(accion);
+    }
+
+}
