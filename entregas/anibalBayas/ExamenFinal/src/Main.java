4cbed94734a63dd07fb5f2827906bf4bfab1e245
anibalbg <114480424+anibalbg@users.noreply.github.com>
2024-01-24 13:35:11 +0100

    Delete entregas/anibalBayas/ExamenFinal/src directory

--- entregas/anibalBayas/ExamenFinal/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Gestion gestion = new Gestion();
-        gestion.capturarDatos();
-        gestion.mostrar();
-        gestion.estadisticas();
-    }
-}
