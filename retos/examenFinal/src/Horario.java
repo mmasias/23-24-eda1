08184e430752f737819c2c74c35fa8ca0f1071d3
DavidGarciaCosta <114480378+DavidGarciaCosta@users.noreply.github.com>
2024-01-24 13:57:15 +0100

    Add files via upload

---
+++ retos/examenFinal/src/Horario.java
@@ -0,0 +1,28 @@
+public enum Horario {
+    DESAYUNO(1,"Desayuno"),
+    MEDIAMAÑANA(2, "Media Mañana"),
+    ALMUERZO(3, "Almuerzo"),
+    MERIENDA(4, "Merienda"),
+    CENA(5, "Cena");
+
+    private int codigo;
+    private String description;
+    private Horario(int code, String description){
+        this.codigo=code;
+        this.description=description;
+
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
