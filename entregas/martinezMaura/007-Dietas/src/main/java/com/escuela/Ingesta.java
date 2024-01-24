283413f65a2ca36d130df8194fcffd634e4057c2
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-01-24 11:59:16 +0100

    Update Ingesta.java

--- entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Ingesta.java
+++ entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Ingesta.java
@@ -1,20 +1,42 @@
 package com.escuela;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 class Ingesta {
     String tipo;
-    Map<String, Alimento> alimentos;
+    Alimento raiz;
+
+    Ingesta izquierda;
+    Ingesta derecha;
 
     public Ingesta(String tipo) {
         this.tipo = tipo;
-        this.alimentos = new TreeMap<>();
+        this.raiz = null;
+        this.izquierda = null;
+        this.derecha = null;
     }
 
-    public void agregarAlimento(String nombre) {
-        Alimento alimento = new Alimento(nombre);
-        alimentos.put(nombre, alimento);
+    public void registrarAlimento(String nombre) {
+        if (raiz == null) {
+            raiz = new Alimento(nombre);
+        } else {
+         
+            System.out.println("Ya hay un alimento registrado en esta ingesta.");
+        }
+    }
+
+    public void mostrarIngesta() {
+        System.out.println("  " + tipo + ":");
+        if (raiz != null) {
+            mostrarAlimento(raiz);
+        } else {
+            System.out.println("      Sin alimentos registrados.");
+        }
+    }
+
+    private void mostrarAlimento(Alimento nodo) {
+        if (nodo != null) {
+            mostrarAlimento(nodo.izquierda);
+            System.out.println("      " + nodo.nombre);
+            mostrarAlimento(nodo.derecha);
+        }
     }
 }
-
