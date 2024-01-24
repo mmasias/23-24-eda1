74d6cc1eb4beb9c2ed59d2e1e0ca327bec6221b3
recinosjer <josemanuelrecinos@gmail.com>
2024-01-24 13:12:54 +0100

    fix/node and Encuesta for list

--- entregas/RecinosJose/ExamenFinal/Node.java
+++ entregas/RecinosJose/ExamenFinal/Node.java
@@ -4,15 +4,11 @@ public class Node<T> {
     private T data;
     private int index;
     private Node<T> next;
-    private Node<T> left;
-    private Node<T> right;
 
     public Node(T data, int index) {
         this.data = data;
         this.index = index;
         this.next = null;
-        this.left = null;
-        this.right = null;
     }
 
     public int getIndex() {
@@ -21,22 +17,6 @@ public class Node<T> {
 
     public T getData() {
         return data;
-    }
-
-    public Node<T> getLeft() {
-        return left;
-    }
-
-    public void setLeft(Node<T> left) {
-        this.left = left;
-    }
-
-    public Node<T> getRight() {
-        return right;
-    }
-
-    public void setRight(Node<T> right) {
-        this.right = right;
     }
 
     public Node<T> getNext() {
