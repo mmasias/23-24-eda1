7fed0abb610e13b85925ed3a10157230861b4146
Maura Martinez <137115639+Martinez-rosa@users.noreply.github.com>
2024-01-24 12:00:28 +0100

    Update Alimento.java

--- entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Alimento.java
+++ entregas/martinezMaura/007-Dietas/src/main/java/com/escuela/Alimento.java
@@ -3,7 +3,13 @@ package com.escuela;
 class Alimento {
     String nombre;
 
+    Alimento izquierda;
+    Alimento derecha;
+
     public Alimento(String nombre) {
         this.nombre = nombre;
+        this.izquierda = null;
+        this.derecha = null;
     }
 }
+
