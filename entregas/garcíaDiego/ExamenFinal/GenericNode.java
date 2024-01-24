0e287f8b619df0ea14cc2b0bafb223c541d0e180
Dievexx <diego30sscc@gmail.com>
2024-01-24 12:00:52 +0100

    creacion del proyecto del examen final

---
+++ entregas/garcíaDiego/ExamenFinal/GenericNode.java
@@ -0,0 +1,25 @@
+public class GenericNode<T> {
+
+    private T value;
+    private GenericNode<T> next;
+
+    public GenericNode(T value) {
+        this.setValue(value);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T newValue) {
+        this.value = newValue;
+    }
+
+    public GenericNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(GenericNode<T> next) {
+        this.next = next;
+    }
+}
