a2b4c21411c19da17eb6a980cf13d35117fd22c2
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2024-01-24 13:43:55 +0100

    UltimoCommit

---
+++ retos/examenFinal/Alimento.java
@@ -0,0 +1,34 @@
+public class Alimento implements Comparable<Alimento> {
+    private String nombre;
+    private int gramos;
+    
+    public Alimento(String nombre, int gramos) {
+        this.nombre = nombre;
+        this.gramos = gramos;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getGramos() {
+        return gramos;
+    }
+    public void setGramos(int gramos) {
+        this.gramos = gramos;
+    }
+   
+    @Override
+    public int compareTo(Alimento o) {
+        if (this.getNombre().compareTo(o.getNombre()) > 0){
+            return 1;
+        }else if(this.getNombre().compareTo(o.getNombre()) < 0){
+          return -1;
+        }else{
+            return 0;
+        }
+    }
+    
+    
+}
---
+++ retos/examenFinal/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Gestion gestion = new Gestion(null);
+        gestion.capturarDatos();
+        gestion.mostrar();
+        gestion.Estadisticas();
+       
+
+    }
+}
+
+
